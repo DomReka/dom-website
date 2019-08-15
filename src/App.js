ee14104a848f9113c6a8c3fe2a16c3ef65279f76
DomReka <dominiquebosman02@gmail.com>
2019-08-15 10:45:04 +0200

    home css responsive and start about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Home from './components/home';
+import About from './components/about';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Home></Home>
+      <About></About>
     </div>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
 import './home';
 
 class Home extends Component {
     render(){
         return(
-            <div className="container-fluid">
+            <div className="container-fluid homeWrapper">
                 <div className="myPix">
                     <img className="pixOfMe" src={require('../images/Me.jpg')} alt="Me"/>
                 </div>
                 <div className="quote">
                     {/* <q>Be grateful or be great, fool.</q> */}
-                    <q>Hey there, <br />I'm Dom, and I'm a Web Developer</q>
+                    <q>Hey there, I'm Dom, and I'm a Web Developer</q>
                 </div>
             </div>
         )
