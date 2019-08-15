07aeed7353784b11f8cf30ea65a7c3b05df6acf6
DomReka <dominiquebosman02@gmail.com>
2019-08-15 11:27:24 +0200

    add some info in about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class About extends Component {
-    state = {  }
-    render() { 
-        return ( 
+    state = {}
+    render() {
+        return (
             <div className="container-fluid aboutWrapper">
-
+                <div className="container aboutMe">
+                    <h5>About me</h5>
+                    <p>
+                        Imagine a regular guy, who grew up with music, IT stuff, and learned everything
+                        he can until he can mixing it up to be who he is right now. That's me, I do all
+                        I can with passion at the maximum of my capacities.
+                    </p>
+                </div>
             </div>
         );
     }
